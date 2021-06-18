31ac45c724f50f0416307f4f50b1947e2858fc84
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-06-18 15:54:25 -0700

    Added site background , working on routes

---
+++ backend/db/migrations/20210618223106-create-note.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      notebookId: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.TEXT
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Notes');
+  }
+};
