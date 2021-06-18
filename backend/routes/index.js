e2854c61056c968806cfc9714b7c18e2d0b2dbec
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-06-18 09:00:42 -0700

    Adding root json for hreoku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,14 +4,14 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-
-// Serve the static assets in the frontend's build folder
+// Static routes
+// Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     // Serve the frontend's index.html file at the root route
     router.get('/', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -22,21 +22,18 @@ if (process.env.NODE_ENV === 'production') {
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
 }
+
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        res.status(201).json({});
     });
 }
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
-
-module.exports = router; 2
+module.exports = router;
