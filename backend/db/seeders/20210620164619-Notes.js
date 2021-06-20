af4ebd210f1d61ca9738a24590e59f787682f286
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-06-20 14:11:31 -0700

    Complete db models and assoc.
    Completed database models and associations as well as made dummy seeder data to test my notebook component if it is retrievable

--- backend/db/seeders/20210620164619-Notes.js
+++ backend/db/seeders/20210620164619-Notes.js
@@ -2,13 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('People', [{
-      name: 'John Doe',
-      isBetaMember: false
+    return queryInterface.bulkInsert('Notes', [{
+      name: 'a/A code of conduct',
+      notebookId: 2,
+      description: 'This is a story all about how my life',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('People', null, {});
+    return queryInterface.bulkDelete('Notes', null, {});
   }
 };
