dce5f2680ee37d8e45e66efe43d5829111e2e959
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-03-21 21:34:53 -0700

    update to dependencies

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,22 @@
 const config = require('./index');
+require('dotenv').config()
 
 const db = config.db;
+
 const username = db.username;
+// const username = process.env.DB_USERNAME
 const password = db.password;
+// const password = process.env.DB_PASSWORD
 const database = db.database;
+// const database = process.env.DB_DATABASE
 const host = db.host;
+// const host = process.env.DB_HOST
+
+console.log('username', process.env.DB_USERNAME);
+console.log('password', process.env.DB_PASSWORD);
+console.log('database', process.env.DB_DATABASE);
+console.log('host', host);
+
 
 module.exports = {
   development: {
