7ec36ca8c1cb8c989cfc7f75ec57b29ea60f172a
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-03-23 18:22:17 -0700

    debugging

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -16,7 +16,7 @@ export async function csrfFetch(url, options = {}) {
     }
     // call the default window's fetch with the url and the options passed in
     const res = await window.fetch(url, options);
-    console.log(options)
+    console.log("options",options)
 
     // if the response status code is 400 or above, then throw an error with the
     // error being the response
