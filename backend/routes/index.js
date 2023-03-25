3c66db3db00ae3f6b20a1897d6cbcd8598d0c0d7
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-03-24 20:50:41 -0700

    edits to css and fixing csrf issues

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -32,10 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
-        print(req)
-        print(res)
         res.cookie('_csrf', req.csrfToken());
         res.status(201).json({});
+        console.log(res)
     });
 }
 
