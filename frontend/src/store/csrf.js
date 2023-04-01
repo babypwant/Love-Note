f29d127b4c2d0f2de30c0bf3059f088a09999a7f
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-03-31 19:43:30 -0700

    updated csrf

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -7,22 +7,22 @@ export async function csrfFetch(url, options = {}) {
   options.headers = options.headers || {};
 
   // if the options.method is not 'GET', then set the "Content-Type" header to
-  // "application/json", and set the "_csrf" header to the value of the 
-  // "_csrf" cookie
+  // "application/json", and set the "XSRF-TOKEN" header to the value of the 
+  // "XSRF-TOKEN" cookie
   if (options.method.toUpperCase() !== 'GET') {
     options.headers['Content-Type'] =
       options.headers['Content-Type'] || 'application/json';
-    // options.headers['_csrf'] = 'K1jXYgoJ-au5gjLh5MxGFZD-FT57Rr4nmkwY'
-    options.headers['_csrf'] = Cookies.get('_csrf')
+    // options.headers['XSRF-TOKEN'] = 'K1jXYgoJ-au5gjLh5MxGFZD-FT57Rr4nmkwY'
+    options.headers['XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN')
     
     // TfcBIvFcOYAVyGH0QkSqj2CI  
 
   }
 
-  const csrfToken = Cookies.get('_csrf');
+  const csrfToken = Cookies.get('XSRF-TOKEN');
 
-  console.log('csrfToken', csrfToken)
-  console.log(options.headers['_csrf'])
+  console.log('XSRF-TOKEN', csrfToken)
+  console.log(options.headers['XSRF-TOKEN'])
 
   // call the default window's fetch with the url and the options passed in
   const res = await window.fetch(`http://localhost:8080${url}`, options);
